9c91afe1f3c644c89af0145da2fcd88999631f4d
paul-c-hartman <real.paul.hartman@gmail.com>
2024-10-09 20:10:00 -0700

    Bring into compliance with Rubocop

--- bin/runner.rb
+++ bin/runner.rb
@@ -1,13 +1,14 @@
-#
+# frozen_string_literal: true
+
 # Usage:
 #   ./bin/runner.rb [filename] [args]
 #
 # Simply passes through all cmdline arguments to the runner class in lib/run.rb.
 #
 
-# FIXME Figure out where to extract config options
+# FIXME: Figure out where to extract config options
 
-require "bundler/setup"
-require_relative "../lib/run"
+require 'bundler/setup'
+require_relative '../lib/run'
 
 Shoes::Runner.new(*ARGV.to_a).run
--- lib/run.rb
+++ lib/run.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 module Shoes
+  # Shoes::Runner is instantiated by the run-script and handles launching and running the interpreter modules.
   class Runner
-    def initialize(filename = "", *options)
-      @filename, @options = filename, options
+    def initialize(filename = '', *options)
+      @filename = filename
+      @options = options
     end
 
     # TODO: define #parse_opts (use optparse), call in #initialize
